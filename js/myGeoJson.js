9f7217c7297df3670efec3e2824f5ca06fbf0235
Jasig21 <96052520+Jasig21@users.noreply.github.com>
2022-11-23 16:02:14 +0100

    Add files via upload

---
+++ js/myGeoJson.js
@@ -0,0 +1,280 @@
+var myGeoJson = {
+  "type" : "FeatureCollection",
+  "features" : [
+    {
+      "type" : "Feature",
+      "id" : 1,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          4.2001784581308348,
+          36.580615442619951,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 1,
+        "Etablissement" : "Lycée Si Hacen Outaleb",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660334512000,
+        "Evenement" : "BAC S",
+        "Type" : "Formation",
+        "Lieu" : "Béni Yenni ",
+        "Image" : "https://upload.wikimedia.org/wikipedia/fr/c/cd/Logo_Sorbonne_Universit%C3%A9.png",
+        "Periode" : "Juin 2017"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 2,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          3.1804062269515065,
+          36.712407104773156,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 2,
+        "Etablissement" : "Université des Sciences et de la Technologie Houari Boumediene",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332178000,
+        "Evenement" : "Licence géographie et aménagement du territoire ",
+        "Type" : "Formation",
+        "Lieu" : "Bab Ezzouar - Alger",
+        "Image" : null,
+        "Periode" : "Septembre 2017 - Juin 2020"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 3,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          3.2297357114907412,
+          34.677182281899626,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 3,
+        "Etablissement" : "Université des Sciences et de la Technologie Houari Boumediene",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332203000,
+        "Evenement" : "Stage de terrain ",
+        "Type" : "Formation",
+        "Lieu" : "Bab Ezzouar - Alger",
+        "Image" : null,
+        "Periode" : "Avril 2019"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 4,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          2.3427401153344016,
+          48.848237427259292,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 4,
+        "Etablissement" : "Sorbonne université - Paris 4",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332213000,
+        "Evenement" : "3ème année de licence en géographie et aménagement du territoire ",
+        "Type" : "Formation",
+        "Lieu" : "Paris - 15ème ",
+        "Image" : null,
+        "Periode" : "Septembre 2020 - Mai 2021"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 5,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          2.3650930822970473,
+          48.944108522992224,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 5,
+        "Etablissement" : "Université de Saint Denis - Paris 8",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332230000,
+        "Evenement" : "Master de géomatique géodécisionnel, geomarketing et multimédia (G2M)",
+        "Type" : "Formation",
+        "Lieu" : "Saint Denis",
+        "Image" : null,
+        "Periode" : "Aout 2021 - Aout 2023"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 6,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          3.0784150408699662,
+          50.629757534076617,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 6,
+        "Etablissement" : "Direction de la Mer des ports et du littoral - Région Hautsde France",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332233000,
+        "Evenement" : "Apprentissage en géomatique ",
+        "Type" : "Expérience professionnelle",
+        "Lieu" : "Lille",
+        "Image" : null,
+        "Periode" : "Novembre 2021 - Aout 2023"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 7,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          1.8546944486425643,
+          50.961933760947936,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 7,
+        "Etablissement" : "Direction de la Mer des ports et du littoral - Région Hautsde France",
+        "created_user" : null,
+        "created_date" : null,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332238000,
+        "Evenement" : "Apprentissage en géomatique - Missions site portuaire de Calais",
+        "Type" : "Expérience professionnelle",
+        "Lieu" : "Calais",
+        "Image" : null,
+        "Periode" : "Novembre 2021 - Aout 2023"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 8,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          4.0406210295670864,
+          36.717025613547484,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 8,
+        "Etablissement" : "Smayli",
+        "created_user" : "YKHALEF",
+        "created_date" : 1660313188000,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332243000,
+        "Evenement" : "Equipière polyvalente",
+        "Type" : "Job étudiant",
+        "Lieu" : "Tizi Ouzou",
+        "Image" : null,
+        "Periode" : "Eté 2018"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 9,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          2.0613443545671086,
+          49.032748796852779,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 9,
+        "Etablissement" : "Rotisserie O",
+        "created_user" : "YKHALEF",
+        "created_date" : 1660313347000,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332248000,
+        "Evenement" : "Serveuse",
+        "Type" : "Job étudiant",
+        "Lieu" : "Cergy",
+        "Image" : null,
+        "Periode" : "Eté 2021"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 10,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          2.0169969634245617,
+          48.923067665497022,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 10,
+        "Etablissement" : "Association Socio Culturelle et du Logement de la Coudraie",
+        "created_user" : "YKHALEF",
+        "created_date" : 1660313545000,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332252000,
+        "Evenement" : "Soutien scolaire - Lilaby",
+        "Type" : "Entreprenariat",
+        "Lieu" : "Poissy",
+        "Image" : null,
+        "Periode" : "Septembre 2021 - Juin 2022"
+      }
+    },
+    {
+      "type" : "Feature",
+      "id" : 11,
+      "geometry" : {
+        "type" : "Point",
+        "coordinates" : [
+          1.6005188779642294,
+          50.726391056926943,
+          0
+        ]
+      },
+      "properties" : {
+        "OBJECTID" : 11,
+        "Etablissement" : "Direction de la Mer des ports et du littoral - Région Hautsde France",
+        "created_user" : "YKHALEF",
+        "created_date" : 1660332313000,
+        "last_edited_user" : "YKHALEF",
+        "last_edited_date" : 1660332412000,
+        "Evenement" : "Apprentissage en géomatique - Missions site portuaire de Boulogne-sur-Mer",
+        "Type" : "Expérience professionnelle",
+        "Lieu" : "Boulogne-sur-Mer",
+        "Image" : null,
+        "Periode" : "Novembre 2021 - Aout 2023"
+      }
+    }
+  ]
+}
